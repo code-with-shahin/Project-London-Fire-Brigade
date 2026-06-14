ff2c98224b4373acf73971a55d5e6b95fd9c49f9
Shahin Amirov <amirov.shahin@gmail.com>
2026-06-14 23:57:03 +0200

    importing dataset

--- main.py
+++ main.py
@@ -7,9 +7,47 @@ import seaborn as sns
 import statsmodels.api as sm
 import os
 
-incidents = pd.read_csv(
-    "data/Incidents/LFB Incident data from 2009 - 2017.csv",
-    low_memory=False
-)
+# LOAD INCIDENT DATA
 
+incident_files = [
+    "data/Incidents/LFB Incident data from 2018 - 2023.xlsx",
+    "data/Incidents/LFB Incident data from 2009 - 2017.csv"
+]
+
+incident_dfs = []
+
+for file in incident_files:
+    if file.endswith(".csv"):
+        df = pd.read_csv(file, low_memory=False)
+    else:
+        df = pd.read_excel(file)
+
+    incident_dfs.append(df)
+
+incidents = pd.concat(incident_dfs, ignore_index=True)
+
+print("Incidents shape:", incidents.shape)
 print(incidents.head())
+
+# LOAD MOBILISATION DATA
+
+mobilisation_files = [
+    "data/Mobilisation/LFB Mobilisation data from 2015 - 2020.xlsx",
+    "data/Mobilisation/LFB Mobilisation data from 2021 - 2024.csv"
+]
+
+mobilisation_dfs = []
+
+for file in mobilisation_files:
+    if file.endswith(".csv"):
+        df = pd.read_csv(file, low_memory=False)
+    else:
+        df = pd.read_excel(file)
+
+    mobilisation_dfs.append(df)
+
+mobilisation = pd.concat(mobilisation_dfs, ignore_index=True)
+
+print("Mobilisation shape:", mobilisation.shape)
+print(mobilisation.head())
+
