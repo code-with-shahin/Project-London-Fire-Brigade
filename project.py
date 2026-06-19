b3d3efa6d1aaec78b7c17df76d48598bd911eb31
Shahin Amirov <amirov.shahin@gmail.com>
2026-06-19 11:22:55 +0200

    exploration

--- project.py
+++ project.py
@@ -245,4 +245,48 @@ df.shape
 
 df = df.loc[(df["IncGeo_BoroughName"] == "WESTMINSTER") | (df["IncGeo_BoroughName"] == "HAVERING")]
 
+# create summary table
+
+# define parameters for summary table
+summary = pd.DataFrame({
+    "Column Name": df.columns,
+    "Data Type": df.dtypes.values,
+    "Missing values": df.isna().sum().values,
+    "Missing values in %": (df.isnull().sum().values) / len(df) * 100,
+    "Unique values": df.nunique().values
+})
+
+# display number of rows and columns
+rows, cols = df.shape
+
+# display summary table
+print("-------------------------")
+print("Summary table:")
+print("-------------------------\n")
+
+print("Number of rows:", rows)
+print("Number of columns:", cols)
+print("\n-------------------------\n")
+print(summary)
+
+# Modalities
+
+# modalities of columns where number of unique values are less than 20
+
+for col in df.columns:
+  if len(df[col].unique()) < 20:
+        print(f"Modalities of {col}:\n{df[col].unique()}\n")
+
+# Outliers
+
+# detect implausible values regarding response time
+# drop missing values since they haven´t been replaced yet
+no_nans = df.dropna(subset=["FirstPumpArriving_AttendanceTime"])
+
+# create boxplot with seaborn
+plt.figure()
+sns.boxplot(x=no_nans["FirstPumpArriving_AttendanceTime"])
+plt.title("Boxplot of First Pump Attendance Time")
+plt.xlabel("Attendance Time in seconds")
+plt.show()
 
